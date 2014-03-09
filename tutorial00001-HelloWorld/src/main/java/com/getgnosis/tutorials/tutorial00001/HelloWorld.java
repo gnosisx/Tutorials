b212f91541a00cc7021c70a2ca5ab9c16f295df1
gnosis <gnosis@getgnosis.com>
2014-03-09 10:40:10 -0500

    Version 1.0.0 of the project.

---
+++ tutorial00001-HelloWorld/src/main/java/com/getgnosis/tutorials/tutorial00001/HelloWorld.java
@@ -0,0 +1,34 @@
+/**
+ * HelloWorld.java
+ * 2013 GetGnosis.com
+ */
+package com.getgnosis.tutorials.tutorial00001;
+
+/**
+ * This application does one thing.  It prints the {@link String} "Hello World!"
+ * to the system's output display.
+ *
+ * @author mhemphill
+ * @version 1.0.0 - Mar 9, 2014
+ */
+public class HelloWorld {
+
+	/**
+	 * The default constructor.
+	 * Initializes Nothing.
+	 */
+	public HelloWorld() {}
+
+	/**
+	 * The main method for the hello world application.  This method acts as the 
+	 * enter point for the application and prints "Hello World!" to the System's
+	 * output display.
+	 * 
+	 * @param args - {@link String} array instance of inputed parameters. Not
+	 * used for any application function.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
